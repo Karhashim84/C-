6b1bf08978f13cf608867e496a296f125ab05cf7
Karhashim <karhashim84@yandex.ru>
2022-11-27 15:25:55 +0500

    exam7.2

--- exam7/Program.cs
+++ exam7/Program.cs
@@ -1,21 +1,21 @@
 ﻿Console.Clear();
-//Console.SetCursorePosition(10, 4);
+//Console.SetCursorPosition(10, 4);
 //Console.WriteLine("+");
 
-int xa = 1;
+int xa = 40;
 int ya = 1;
 int xb = 1;
 int yb = 30;
-int xc = 40;
+int xc = 80;
 int yc = 30;
 
-Console.SetCursorePosition(xa, ya);
+Console.SetCursorPosition(xa, ya);
 Console.WriteLine("+");
 
-Console.SetCursorePosition(xb, yb);
+Console.SetCursorPosition(xb, yb);
 Console.WriteLine("+");
 
-Console.SetCursorePosition(xc, yc);
+Console.SetCursorPosition(xc, yc);
 Console.WriteLine("+");
 
 int x = xa, y = ya;
