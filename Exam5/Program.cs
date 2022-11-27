ff97749329d445b1dc7f02168cd3d9e67bffc358
Karhashim <karhashim84@yandex.ru>
2022-11-27 14:53:07 +0500

    exam5

---
+++ Exam5/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("ВВедите имя пользователя");
+string username = Console.ReadLine();
+
+if(username == "Маша")
+{
+    Console.WriteLine("Ура, это Маша!");
+}
+else
+{
+    Console.WriteLine("Привет,");
+    Console.WriteLine(username)
+}
